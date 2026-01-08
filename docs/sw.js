9f8407d1e2e7754aca115224a7a191ddca0576f4
simb8info-spec <simb8.info@gmail.com>
2026-01-09 00:15:26 +0100

    Update sw.js

--- docs/sw.js
+++ docs/sw.js
@@ -1,1 +1,29 @@
+const CACHE = "gtts-cache-v1";
+const ASSETS = [
+  "./",
+  "./index.html",
+  "./styles.css",
+  "./app.js",
+  "./manifest.json",
+  "./data/fuel_stations.json",
+  "./data/hotels.json",
+  "./data/pois.json",
+  "./data/toll_demo.json"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+});
+
+self.addEventListener("activate", (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE ? caches.delete(k) : null)))
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
 
